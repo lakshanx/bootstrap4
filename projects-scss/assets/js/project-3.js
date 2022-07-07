5b0578470231293fd1080c09666f97305087afd0
Lakshan Danushka <lakshanx@gmail.com>
2022-07-08 01:50:09 +0530

    scss based theming

---
+++ projects-scss/assets/js/project-3.js
@@ -0,0 +1,35 @@
+$('#year').text(new Date().getFullYear());
+
+$('.slider').slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+  });
+
+
+// Configure Slider
+$('.carousel').carousel({
+  interval: 6000,
+  pause: 'hover'
+});
+
+// Lightbox Init
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+// Video Play
+$(function () {
+  // Auto play modal video
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
+
